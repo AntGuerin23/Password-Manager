a970f9c7a7732c80385d05c873b793ca373d3e8f
Antoine Guérin <90921460+AntGuerin23@users.noreply.github.com>
2022-04-16 21:55:04 -0400

    Feature : Finished index content

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -11,25 +11,44 @@ class MainController extends Controller
         $this->get("/profile", "profile");
         $this->get("/login", "login");
         $this->get("/register", "register");
+        $this->get("/new-password", "newPassword");
     }
 
     public function index()
     {
-        return $this->render("index");
+        return $this->render("index", [
+            "title" => "See your passwords",
+            "location" => "index"
+        ]);
     }
 
     public function login()
     {
-        return $this->render("");
+        return $this->render("login", [
+            "title" => "Login",
+        ]);
     }
 
     public function register()
     {
-        return $this->render("");
+        return $this->render("register", [
+            "title" => "Register"
+        ]);
     }
 
     public function profile()
     {
-        return $this->render("");
+        return $this->render("profile", [
+            "title" => "Manage your profile",
+            "location" => "profile"
+        ]);
+    }
+
+    public function newPassword()
+    {
+        return $this->render("new-password", [
+            "title" => "Add a new password",
+            "location" => "new-password"
+        ]);
     }
 }
